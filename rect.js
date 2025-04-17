b4aaaa47c1ded6361278013ea44c214a5e6061c2
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2025-04-17 19:55:42 +0200

    Create rect.js

---
+++ rect.js
@@ -0,0 +1,22 @@
+class Rect extends PhysicsObject {
+
+    constructor(x, y, w, h, velX, velY, mass) {
+        super(x, y, velX, velY, mass);
+        this.w = w;
+        this.h = h;
+
+    }
+
+    draw() {
+        rect(this.pos.x, this.pos.y, this.w, this.h);
+    }
+
+    wallCollision() {
+        if(this.pos.x > width - this.w || this.pos.x < 0) {
+            this.vel.x = -this.vel.x;
+        }
+        if(this.pos.y > height - this.h || this.pos.y < 0) {
+            this.vel.y = -this.vel.y;
+        }
+    }
+}

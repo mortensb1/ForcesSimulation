facaafd2d4a648d6e7cc6c5a339694ddcd104e29
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2025-04-17 19:55:23 +0200

    Create vec2.js

---
+++ vec2.js
@@ -0,0 +1,96 @@
+class Vec2 {
+    constructor(x = 0.0, y = 0.0) {
+        this.x = x; 
+        this.y = y;
+    }
+
+    /**
+     * Set Vector to new Vector
+     * @param {Vec2} v new Vector
+     */
+    set(v) {
+        this.x = v.x; this.y = v.y;
+    }
+
+    /**
+     * Return a clone of the Vector 
+     * @returns {Vec2}
+     */
+    clone() {
+        return new Vec2(this.x, this.y);
+    }
+
+    /**
+     * Add scalor to Vector
+     * @param {Vec2} v 
+     * @param {number} s 
+     * @returns {Vec2}
+     */
+    add(v, s = 1.0) {
+        this.x += v.x * s;
+        this.y += v.y * s;
+        return this;
+    }
+
+    /**
+     * Add Vectors
+     * @param {Vec2} a 
+     * @param {Vec2} b 
+     * @returns {Vec2}
+     */
+    addVectors(a, b) {
+        this.x = a.x + b.x;
+        this.y = a.y + b.y;
+        return this;
+    }
+
+    /**
+     * Subtact scalar from Vector
+     * @param {Vec2} v 
+     * @param {number} s 
+     * @returns {Vec2}
+     */
+    subtract(v, s = 1.0) {
+        this.x -= v.x * s;
+        this.y -= v.y * s;
+        return this;
+    }
+
+    /**
+    * Subtract Vectors
+    * @param {Vec2} a 
+    * @param {Vec2} b 
+    * @returns {Vec2}
+    */
+    subtractVectors(a, b) {
+        this.x = a.x - b.x;
+        this.y = a.y - b.y;
+        return this;			
+    }
+
+    /**
+     * Calculate the length
+     * @returns {number}
+     */
+    length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    /**
+     * Scale the Vector
+     * @param {number} s 
+     */
+    scale(s) {
+        this.x *= s;
+        this.y *= s;
+    }
+
+    /**
+     * Calcualte the dot product
+     * @param {Vec2} v 
+     * @returns {number} The dot product
+     */
+    dot(v) {
+        return this.x * v.x + this.y * v.y;
+    }
+}

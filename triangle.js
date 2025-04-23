b876e0f3f522d2f4f35e68f2b9af9015339637df
JosefineM22R <44941483+cpsaj@users.noreply.github.com>
2025-04-23 14:07:34 +0200

    Triangle rotating

--- triangle.js
+++ triangle.js
@@ -1,8 +1,9 @@
 class Triangle extends PhysicsObject {
-    constructor(x, y, h, w, velX, velY, mass, elasticity = 1, angle = 0, rotVel = 0, isStatic = false) {
+    constructor(x, y, p1, p2, p3, velX, velY, mass, elasticity = 1, angle = 0, rotVel = 0, isStatic = false) {
         super(x, y, velX, velY, mass, elasticity, angle, rotVel, isStatic);
-        this.h = h;
-        this.w = w;
+        this.startP1 = p1;
+        this.startP2 = p2;
+        this.startP3 = p3;
 
         this.corners = {
             p1: new Vec2(),
@@ -11,7 +12,33 @@ class Triangle extends PhysicsObject {
         }
     }
 
-    updateCorners() {
-        
+    draw() {
+        push();
+        translate(this.pos.x + width / 2, height / 2 - this.pos.y);
+        rotate(-this.angle);
+        triangle(this.startP1.x, -this.startP1.y, this.startP2.x, -this.startP2.y, this.startP3.x, -this.startP3.y);
+        pop();
     }
+
+    updateCorners() {
+        this.corners.p1.x = this.Cos * (this.startP1.x) - this.Sin * (this.startP1.y);
+        this.corners.p1.y = this.Sin * (this.startP1.x) + this.Cos * (this.startP1.y);
+        this.corners.p1.add(this.pos);
+
+        this.corners.p2.x = this.Cos * (this.startP2.x) - this.Sin * (this.startP2.y);
+        this.corners.p2.y = this.Sin * (this.startP2.x) + this.Cos * (this.startP2.y);
+        this.corners.p2.add(this.pos);
+
+        this.corners.p3.x = this.Cos * (this.startP3.x) - this.Sin * (this.startP3.y);
+        this.corners.p3.y = this.Sin * (this.startP3.x) + this.Cos * (this.startP3.y);
+        this.corners.p3.add(this.pos);
+    }
+
+    rotate(angleChange) {
+        if (angleChange == 0) return; // Stop if no change
+        this.angle += angleChange;
+        this.Cos = cos(this.angle);
+        this.Sin = sin(this.angle);
+        this.updateCorners();
+      }
 }

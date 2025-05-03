e8a984c3ca78ae4c23cc17e846d8e58265dd658c
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2025-05-03 15:06:50 +0200

    Manifold Class

--- collision.js
+++ collision.js
@@ -258,25 +258,3 @@ function drawNormalsRect(rect) {
   }
 }
 
-function colCirCir(cir1, cir2) {}
-
-function colRectCir(rect, cir) {
-  if (cir.pos.y > rect.pos.y + rect.h) {
-    closeY = rect.pos.y + rect.h;
-  } else if (cir.pos.y < rect.pos.y) {
-    closeY = rect.pos.y;
-  }
-  if (cir.pos.x > rect.pos.x + rect.w) {
-    closeX = rect.pos.x + rect.w;
-  } else if (cir.pos.x < rect.pos.x) {
-    closeX = rect.pos.x;
-  }
-
-  let dis = (closeX - cir.pos.x) ** 2 + (closeY - cir.pos.y) ** 2;
-  if (dis < cir.r ** 2) {
-    if (closeY == rect.pos.y + rect.h) {
-      if (closeX == rect.pos.x + rect.w) {
-      }
-    }
-  }
-}

a9c973b7e91277b4c95a7a1f8342de7d3f075c35
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2025-05-04 00:41:57 +0200

    Cross product for Vec2

--- vec2.js
+++ vec2.js
@@ -14,6 +14,15 @@ class Vec2 {
     }
 
     /**
+     * Calculate the z value of the Cross product as the z of the vectors must be 0
+     * @param {Vec2} v
+     */
+    cross(v) {
+        let z = this.x * v.y - v.x * this.y;
+        return z;
+    }
+
+    /**
      *
      * @returns {Vec2} Returns the Cross vector (Rotated 90°)
      */
@@ -110,6 +119,7 @@ class Vec2 {
     scale(s) {
         this.x *= s;
         this.y *= s;
+        return this;
     }
 
     /**
